ffbf9f8187493247cbdf464bf13b732766f766d8
Alexandra1234 <alexandra.zakovinko>
2022-09-29 18:17:31 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+* {
+  font-family: Inter, serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+h1, h2, h3, h4, h5, h6, ul {
+  padding: 0;
+  margin: 0;
+}
+
+input, textarea, button {
+  outline: none;
+}
+</style>
